8b170127564096075a7547504362dceec226bbc5
ualon1954 <144025697+ualon1954@users.noreply.github.com>
2024-11-05 18:56:31 +0200

    Add files via upload

--- scripts/validation_kivua.js
+++ scripts/validation_kivua.js
@@ -52,6 +52,9 @@ form1.addEventListener("submit", (e) => {
   if (isValid) {
     //alert("addData");
     updateData();
+    setTimeout(() => {
+      console.log("Delayed for 2 second.");
+    }, "2000");
     updateDataMadad();
     calculateKivua();
     //updateData();
