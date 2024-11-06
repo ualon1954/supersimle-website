ee89f513ddab843bbc86b5fe5eeedd8c5e01f672
ualon1954 <144025697+ualon1954@users.noreply.github.com>
2024-11-06 14:43:55 +0200

    Add files via upload

--- scripts/kivua.js
+++ scripts/kivua.js
@@ -32,8 +32,7 @@ function updateDataMadad() {
     let toMonth = new Date("jan 1, 2024").getMonth()+1;
     let fromDay = new Date(form[3].value).getDate();
     let toDay = new Date("jan 1, 2024").getDate();
-    alert(toYear);
-    
+        
     if (fromMonth === 2 && fromDay < 16) {
         fromYear = fromYear - 1;
         fromMonth = 12; 
